5ee8247a124778f58df19d15b1d7d104e09388ff
Prasad <tandelprasad01@gmail.com>
2025-09-26 20:22:59 +0530

    changes in package json

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'expo',
+    '@react-native',
+  ],
+  rules: {
+    // Add any custom rules here
+  },
+  env: {
+    'react-native/react-native': true,
+  },
+};
